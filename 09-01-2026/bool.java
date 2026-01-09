ff8b5c39d6362d6f6a1e51d267e679c41361593b
Shounak <ShhounakChatterjee@gmail.com>
2026-01-09 13:13:35 +0530

    Add initial boolean variable declaration

---
+++ 09-01-2026/bool.java
@@ -0,0 +1,16 @@
+//Biswajit halder sir program ta likhiyeche ami eto nonsense code likhtam na 
+public class bool {
+    public static void main(String args[]){
+    boolean b;
+    b = false;
+    System.out.println(b);
+    b = true;
+    System.out.println(b);
+    if(b){
+    System.out.println(b);
+    }
+    else{
+    System.out.println(b);
+    }
+    }
+}
