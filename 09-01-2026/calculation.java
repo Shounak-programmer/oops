945000fe85cb6be93b4afcc9fcd3cd4e7dd6d95e
Shounak <ShhounakChatterjee@gmail.com>
2026-01-09 13:14:01 +0530

    Add calculation class to compare three integers

---
+++ 09-01-2026/calculation.java
@@ -0,0 +1,14 @@
+public class calculation {
+   public static void main(String args[]){
+    int a=10;
+    int b=20;
+    int c=15;
+    if(a>b && a>c){
+        System.out.println("a is greater");
+    }else if(b>a && b>c){
+        System.out.println("b is greater");
+    }else{
+        System.out.println("c is greater");
+    }
+    }
+}
