d7c02757293c8ca2e6bd41c679f4dbd1bf9d46e5
Shounak <ShhounakChatterjee@gmail.com>
2026-01-09 13:15:17 +0530

    Add show_name_age class to display student info

---
+++ 09-01-2026/show_name_age.java
@@ -0,0 +1,15 @@
+public class show_name_age {
+    public static void main(String args[]){
+            display(1,"Shounak",18);
+            display(2,"Safidul",19);
+            display(3,"Arnab",18);
+
+    }
+    public static void display(int iteration, String name, int age){
+        System.out.println("==============Student "+iteration+"==============");
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+
+    }
+    
+}
