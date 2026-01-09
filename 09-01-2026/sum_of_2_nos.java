bee25e2c0b6d0a23b680c2a2efeabce487ece4cd
Shounak <ShhounakChatterjee@gmail.com>
2026-01-09 13:16:10 +0530

    Add sum_of_2_nos.java to calculate sums

---
+++ 09-01-2026/sum_of_2_nos.java
@@ -0,0 +1,13 @@
+public class sum_of_2_nos {
+
+    public static void sum(int a, int b){
+        System.out.println("Sum is: " + (a+b));
+    }
+    public static void sum(double a, double b){
+        System.out.println("Sum is: "+ (a+b));
+    }
+    public static void main(String args []){
+        sum(5,6);
+        sum(6.8,9.1);
+    }
+}
