26e197e70701e9dd3c80e7fbe2e66b0580082442
Shounak <ShhounakChatterjee@gmail.com>
2026-01-30 13:14:25 +0530

    Add Rectangle class and area calculation example
    Implement Rectangle class with area calculation and inheritance.

---
+++ 30-01-2026/area_of_rectangle.java
@@ -0,0 +1,34 @@
+class Rectangle 
+{
+    protected double length;
+    protected double width;
+
+    public Rectangle(double length, double width) 
+    {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double calculateArea() 
+    {
+        return length * width;
+    }
+}
+
+class AreaRectangle extends Rectangle 
+{
+    public AreaRectangle(double length, double width) {
+        super(length, width);
+    }
+
+    public void displayArea() {
+        System.out.println("Area of Rectangle: " + calculateArea());
+    }
+}
+
+public class area_rectangle_inheritance {
+    public static void main(String[] args) {
+        AreaRectangle rect = new AreaRectangle(5.0, 3.0);
+        rect.displayArea();
+    }
+}
